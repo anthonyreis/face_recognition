7831bd469bf23140e8164b02da36e6cd9be58ac0
anthonyreis <matheus.anthony1@gmail.com>
2021-05-06 00:04:22 -0300

    organizacao pastas

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import cv2 as cv
 import os
 
-face_classifier = cv.CascadeClassifier('HaarCascade/haarcascade_frontalface_alt.xml')
+face_classifier = cv.CascadeClassifier('haar_cascade/haarcascade_frontalface_alt.xml')
 
-dir = "document_images"
+dirOrig = "original_images"
+dirResult = "result_images"
 
-caminhos = [os.path.join(dir, nome) for nome in os.listdir(dir)]
+caminhos = [os.path.join(dirOrig, nome) for nome in os.listdir(dirOrig)]
 arquivos = [arq for arq in caminhos if os.path.isfile(arq)]
 pngs = [arq for arq in arquivos if arq.lower().endswith(".png")]
 
@@ -19,7 +20,7 @@ for img in pngs:
     faces = face_classifier.detectMultiScale(image, 2, 3)
    
     for (x, y, w, h) in faces:
-        cv.imwrite(dir + '/' + filename, image[y:y + h, x:x + w])
+        cv.imwrite(dirResult + '/' + filename, image[y:y + h, x:x + w])
         img_rect = cv.rectangle(image, (x, y), (x + w, y + h), (255, 0, 0), 2)
         cv.imshow("Face Detected", img_rect)
         cv.waitKey(0)
